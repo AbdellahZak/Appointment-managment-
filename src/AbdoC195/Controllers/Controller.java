998a0f134d2f3fbf147cde8312df9df3bcf8000f
AbdellahZak <skay180@gmail.com>
2023-02-23 09:12:32 -0700

    DB add: JDBC file, Main: JDBC.makeconnection/closeconnection

--- src/AbdoC195/Controllers/Controller.java
+++ src/AbdoC195/Controllers/Controller.java
@@ -1,11 +1,20 @@
 package AbdoC195.Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
     public void loginScreenLogingIdTxtField(ActionEvent actionEvent) {
     }
 
     public void loginScreenPasswordTxtField(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
