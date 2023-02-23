cf6e2860c8166695abe2a0e9edbee1cb77a041ca
AbdellahZak <skay180@gmail.com>
2023-02-23 07:01:33 -0700

    Initial commit

---
+++ src/AbdoC195/Controllers/Controller.java
@@ -0,0 +1,11 @@
+package AbdoC195.Controllers;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void loginScreenLogingIdTxtField(ActionEvent actionEvent) {
+    }
+
+    public void loginScreenPasswordTxtField(ActionEvent actionEvent) {
+    }
+}
