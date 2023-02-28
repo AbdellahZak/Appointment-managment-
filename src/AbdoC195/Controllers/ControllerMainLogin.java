5605ad9411fdfb9ce08f989b5b5aa857ac8af4d6
AbdellahZak <skay180@gmail.com>
2023-02-28 06:17:10 -0700

    DB add: JDBC file, Main: JDBC.makeconnection/closeconnection

--- src/AbdoC195/Controllers/ControllerMainLogin.java
+++ src/AbdoC195/Controllers/ControllerMainLogin.java
@@ -5,18 +5,26 @@ import AbdoC195.Model.Users;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.sql.SQLException;
+import java.time.ZoneId;
+import java.util.Locale;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
+
 
 public class ControllerMainLogin implements Initializable {
     public Label loginScreenLogingIdLbl;
     public Label loginScreenPasswordLbl;
     public TextField loginScreenLogingIdTxtFieldStat;
     public TextField loginScreenPasswordTxtFieldStat;
+    public TextField mainViewTimeZoneTxtField;
+    public Button loginScreenEnterBtnStat;
+    public Label loginScreenZoneIdLbl;
 
     public void loginScreenLogingIdTxtField(ActionEvent actionEvent) throws SQLException {
 
@@ -37,6 +45,18 @@ public class ControllerMainLogin implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        ZoneId zoneId = ZoneId.systemDefault();  //detcting default Zone Id
+        mainViewTimeZoneTxtField.setText(zoneId.getId());
+        try {
+        ResourceBundle ResBundle= ResourceBundle.getBundle("AbdoC195/Res", Locale.getDefault()); //detecting default language
+        if(Locale.getDefault().equals("fr")){
+            loginScreenZoneIdLbl.setText(ResBundle.getString("ZoneId"));
+        }
+       }catch (MissingResourceException e){
+         }
 
+    }
+
+    public void loginScreenEnterBtn(ActionEvent actionEvent) {
     }
 }
