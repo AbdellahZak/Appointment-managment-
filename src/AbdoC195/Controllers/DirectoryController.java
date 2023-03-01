85a59d0e71c36f1aac37006162617db52bb98c0e
AbdellahZak <skay180@gmail.com>
2023-03-01 07:38:22 -0700

    DB add: JDBC file, Main: JDBC.makeconnection/closeconnection

--- src/AbdoC195/Controllers/DirectoryController.java
+++ src/AbdoC195/Controllers/DirectoryController.java
@@ -1,11 +1,56 @@
 package AbdoC195.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DirectoryController implements Initializable {
+    public TableView directoryViewCustomerTable;
+    public TableColumn directoryViewCustomerIdClmn;
+    public TableColumn directoryViewCustomerAddressClmn;
+    public TableColumn directoryViewCustomerPostalCodeClmn;
+    public TableColumn directoryViewCustomerPhoneNumberClmn;
+    public TableColumn directoryViewCustomerFirstLevelDClmn;
+    public TableColumn directoryViewCutomerCountryClmn;
+    public TableView directoryViewAppTable;
+    public TableColumn directoryViewAppointmentIdClmn;
+    public TableColumn directoryViewAppTitleClmn;
+    public TableColumn directoryViewAppLocationClmn;
+    public TableColumn directoryViewAppContactClmn;
+    public TableColumn directoryViewAppTypeClmn;
+    public TableColumn directoryViewAppStartDateClmn;
+    public TableColumn directoryViewAppEndDateClmn;
+    public TableColumn directoryViewAppCustomerIdClmn;
+    public TableColumn directoryViewAppUserIdClmn;
+
+
+
+
+    public void directoryViewCustomerAddButton(ActionEvent actionEvent) {
+    }
+
+    public void directoryViewCustomerDeleteButton(ActionEvent actionEvent) {
+    }
+
+    public void directoryViewCustomerUpdateButton(ActionEvent actionEvent) {
+    }
+
+
+
+
+    public void directoryViewAppAddButton(ActionEvent actionEvent) {
+    }
+
+    public void directoryViewAppDeleteButton(ActionEvent actionEvent) {
+    }
+
+    public void directoryViewAppUpdateButton(ActionEvent actionEvent) {
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
