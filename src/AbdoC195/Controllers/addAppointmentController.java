2f478b6bd180b2edd9b1deb69946b6ac73621eca
AbdellahZak <skay180@gmail.com>
2023-03-06 09:21:29 -0700

    appointment view added succefully

--- src/AbdoC195/Controllers/addAppointmentController.java
+++ src/AbdoC195/Controllers/addAppointmentController.java
@@ -1,17 +1,27 @@
 package AbdoC195.Controllers;
 
 import AbdoC195.DB.DbHelper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class addAppointmentController implements Initializable {
+    Stage stage;
+    Parent scene;
     public TextField addAppViewAppIdTxt;
     public TextField addAppViewTitleTxt;
     public TextField addAppViewDescriptionTxt;
@@ -24,6 +34,13 @@ public class addAppointmentController implements Initializable {
     public TextField addAppViewUserIdTxt;
     public DatePicker addAppViewStartDatePicker;
     public DatePicker addAppViewEndDatePicker;
+    public ComboBox addAppViewStartHourPickerCombo;
+    public ComboBox addAppViewStartMinutePickerCombo;
+    public ComboBox addAppViewEndHourPickerCombo;
+    public ComboBox addAppViewEndMinutePickerCombo;
+    ObservableList<String> hours = FXCollections.observableArrayList(); // from the code repository.
+    ObservableList<String> minutes = FXCollections.observableArrayList();
+
 
     public void addAppViewStartTimeCombo(ActionEvent actionEvent) {
     }
@@ -34,10 +51,18 @@ public class addAppointmentController implements Initializable {
     public void addAppViewContactComboStat(ActionEvent actionEvent) {
     }
 
-    public void addAppViewAddBtn(ActionEvent actionEvent) {
+    public void addAppViewAddBtn(ActionEvent actionEvent) throws IOException {
+        stage =(Stage)((Button)actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/AbdoC195/Views/directoryView.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
-    public void addAppViewCancelBtn(ActionEvent actionEvent) {
+    public void addAppViewCancelBtn(ActionEvent actionEvent) throws IOException {
+        stage =(Stage)((Button)actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/AbdoC195/Views/directoryView.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @Override
@@ -47,5 +72,12 @@ public class addAppointmentController implements Initializable {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+        hours.addAll("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11",
+                "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23");
+        minutes.addAll("00", "15", "30", "45");
+        addAppViewStartHourPickerCombo.setItems(hours);
+        addAppViewStartMinutePickerCombo.setItems(minutes);
+        addAppViewEndHourPickerCombo.setItems(hours);
+        addAppViewEndMinutePickerCombo.setItems(minutes);
     }
 }
